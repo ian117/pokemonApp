7eff64a8115d6f82835603bb650c0209820e6597
Mariangel <mariangelacosta.88@gmail.com>
2021-02-10 12:49:09 -0500

    se realiza función de llamado y validacion al api pokemon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 export default function App() {
-  const [search, setSearch] = useState("");
-  const [type, setType] = useState("");
+  const [dataPokemon, setDataPokemon] = useState({
+    arrayPokemon: [],
+    error: false,
+    messageError: "",
+  });
+  const [loading, setLoading] = useState(true);
+  const [pokemonTypes, setPokemonTypes] = useState([]);
 
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-  };
+  useEffect(() => {
+    const getPokemonTypes = async () => {
+      try {
+        const url = `https://pokeapi.co/api/v2/type/`;
+        const res = await axios.get(url);
+        setPokemonTypes(res.data.results);
+        setLoading(false);
+      } catch {
+        console.error("error");
+      }
+    };
+    getPokemonTypes();
+  }, []);
 
-  const doSearch = () => {
-    fetch(`https://pokeapi.co/api/v2/type/${search}/`)
-      .then((response) => response.json())
-      .then((data) => setType(data.name));
-  };
+  useEffect(() => {
+    const getListPokemonByType = async (pokemonTypes, searchType) => {
+      setLoading(true);
+      const pokemonType = pokemonTypes.find((type) => type.name === searchType);
+      const isValidType = !!pokemonType;
+      console.log(pokemonType);
+
+      if (isValidType) {
+        try {
+          const url = pokemonType.url;
+          const res = await axios.get(url);
+          setDataPokemon({
+            arrayPokemon: res.data.pokemon,
+            error: false,
+            messageError: "",
+          });
+        } catch {
+          console.error("error");
+        }
+      } else {
+        setDataPokemon({
+          arrayPokemon: [],
+          error: true,
+          messageError: `Pokemon Type "${searchType}" does not exists`,
+        });
+      }
+      setLoading(false);
+    };
+    if (pokemonTypes.length > 0) {
+      getListPokemonByType(pokemonTypes, "fire");
+    }
+  }, [pokemonTypes]);
 
   return (
     <div className="App">
-      <input onChange={handleSearch} value={search} />
-      <button type="button" onClick={doSearch}>
-        search
-      </button>
-      <div>{type}</div>
+      <h1>Pokemon App</h1>
+      {loading && <p>loading...</p>}
+      {!loading && <p>Componente De Busqueda</p>}
     </div>
   );
 }
